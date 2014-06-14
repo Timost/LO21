d7dda26a0ac2bfaf9d7a1939a56cc7873c9a41ad
Zicouto <corentin.bras@etu.utc.fr>
2014-06-14 20:17:30 +0200

    doc 1

--- Inscription.h
+++ Inscription.h
@@ -4,30 +4,92 @@
 #include "enums.h"
 #include "Semestre.h"
 #include "uv.h"
+/**
+* @file inscription.h
+* @version 1
+* @brief Ce fichier est le header de la classe inscription.
+**/
 
+/**
+ * @brief Inscription class permet de créer des inscription
+ */
 class Inscription
 {
+    /**
+     * @brief uv dans laquelle il y a eu inscription
+     */
     const UV* uv;
+    /**
+     * @brief semestre
+     */
     Semestre semestre;
+    /**
+     * @brief resultat
+     */
     Note resultat;
 public:
+    /**
+     * @brief Inscription constructeur
+     * @param u uv
+     * @param s semestre
+     * @param res resultat
+     */
     Inscription(const UV& u, const Semestre& s, Note res=StringToNote("EC")):uv(&u),semestre(s),resultat(res){}
+
+    /**
+     * @brief getUV getter de UV
+     * @return
+     */
     UV getUV() const { return *uv; }
+
+    /**
+     * @brief getCode
+     * @return retourne le code de l'UV
+     */
     QString getCode() const { return QString(uv->getCode().c_str());}
+
+    /**
+     * @brief getSemestre getter du semestre
+     * @return
+     */
     Semestre getSemestre() const { return semestre; }
+
+    /**
+     * @brief getResultat getter du resultat
+     * @return
+     */
     Note getResultat() const { return resultat; }
+
+    /**
+     * @brief setResultat setter du resultat
+     * @param newres
+     */
     void setResultat(Note newres) { resultat=newres; }
 
+    /**
+     * @brief validee
+     * @return retourne true si l'uv a été validé
+     */
     bool validee()//retourne vrai si une inscription a été validée
     {
         return (resultat.isValidatory());
     }
 
+    /**
+     * @brief display affiche l'inscription
+     */
     void display()
     {
         qDebug()<<"UV : "<<uv->getCode().c_str() <<" Semestre :"<<semestre.getSaison().getNom()<<semestre.getAnnee();//<<" Resultat : "<<getResultat();
     }
 };
+
+/**
+ * @brief operator ==
+ * @param i1
+ * @param i2
+ * @return
+ */
 inline bool operator==(Inscription i1, Inscription i2)
 {
     return ((i1.getUV()==i2.getUV())&&(i1.getSemestre()==i2.getSemestre())&&(i1.getResultat()==i2.getResultat()));
--- database.h
+++ database.h
@@ -15,29 +15,76 @@
 #include "Saison.h"
 #define q2c(string) string.toStdString()
 
+/**
+* @file Categorie.h
+* @version 1
+* @brief Ce fichier est le header de la classe database.
+**/
+
+
 using namespace std;
 
+/**
+ * @brief Database class gère tous les échanges avec la base de données.
+ */
 class Database
 {
+    /**
+     * @brief db base de données
+     */
     QSqlDatabase db;
+    /**
+     * @brief databaseName Chemin vers la base de données à laquelle on souhaite accéder.
+     */
     string databaseName;
 
+    /**
+     * @brief open ouvre la connection à la base de donnée.
+     * @return booleen vrai si cel a fonctionné.
+     */
     bool open()
     {
         return this->db.open();
     }
+
+    /**
+     * @brief close ferme la connection à la db.
+     */
     void close()
     {
         db.close();
     }
 public:
+    /**
+     * @brief query effectue une requete vers la bdd.
+     * @param q requete
+     * @return QSqlQuery resultat de la requete
+     */
     QSqlQuery query(string q);
+
+    /**
+     * @brief Database Constructeur de la classe
+     * @param path chemin vers la bdd sera stocké dans databasename
+     * @param dbname type de base de données. par défaut SQLITE
+     */
     Database(string path, string dbname="QSQLITE");
+
+    /**
+    * @brief détruit la base en fermant la connection.
+    *
+    */
     ~Database(){db.close();}
 
+    /**
+     * @brief The SaverLoader class Classe imbriqué à database gère toutes les sauvegardes et chargement de la base de données.
+     */
     class SaverLoader //gere sauvegarde et chargement de la bdd
     {
         //on recupere tous les manager
+        /**
+         * @brief tUV tFormation tEtudiant tCategorie tNote tSaison tSemestre
+         *        manager sur les différents élément à sauvegarder.
+         */
         TemplateManager<UV>& tUV=TemplateManager<UV>::getInstance();
         TemplateManager<Formation>& tFormation=TemplateManager<Formation>::getInstance();
         TemplateManager<Etudiant>& tEtudiant=TemplateManager<Etudiant>::getInstance();
@@ -46,25 +93,48 @@ public:
         TemplateManager<Saison>& tSaison=TemplateManager<Saison>::getInstance();
         TemplateManager<Semestre>& tSemestre=TemplateManager<Semestre>::getInstance();
         Database& db;
+
+        /**
+         * @brief init initialise une base de données propre avec toutes les tables nécessaires.
+         */
         void init();
 
     public:
+        /**
+         * @brief SaverLoader constructeur
+         * @param database Database&
+         */
         SaverLoader(Database& database):db(database){}
+        /**
+         * @brief save enregistre toutes les entités des managers dans une base de données.
+         */
         void save();
+        /**
+         * @brief load charge toutes les entités dans les managers respectifs.
+         */
         void load();
     };
 
+    /**
+     * @brief save appelle le save du saverLoader
+     */
     void save()
     {
         SaverLoader(*this).save();
     }
 
+    /**
+     * @brief load appelle le load du saver loader.
+     */
     void load()
     {
         SaverLoader(*this).load();
     }
 };
 
+/**
+ * @brief DatabaseException class permet d'envoyer des exceptions de la base de données.
+ */
 class DatabaseException : public std::exception
 {
 protected :
